4e756427022f2bd3facc99ae2190c36a1fda3ea7
Acquite <acquitefx@gmail.com>
2023-04-05 02:27:29 +0100

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,149 @@
-import { Plugin, registerPlugin } from 'enmity/managers/plugins';
-import { React } from 'enmity/metro/common';
-import { getByProps } from 'enmity/metro';
-import { create } from 'enmity/patcher';
-import manifest from '../manifest.json';
+import { Plugin, registerPlugin } from 'enmity/managers/plugins'; 
+import { create } from 'enmity/patcher'; 
+import manifest from '../manifest.json'; 
+import { React, StyleSheet } from 'enmity/metro/common'; 
+import { FormArrow, FormDivider, FormRow, FormSection, View } from 'enmity/components'; 
+import { bulk, filters } from 'enmity/metro';
+ import { getIDByName } from 'enmity/api/assets';
+import { findInReactTree } from 'enmity/utilities';
+import { Constants } from 'enmity/metro/common';
 
-import Settings from './components/Settings';
+const Patcher = create(manifest.name);
 
-const Typing = getByProps('startTyping');
-const Patcher = create('silent-typing');
+const [
+   Screens,
+   Titles,
+   SettingsOverviewScreen,
+   UserStore,
+   getScreens,
+   Locale,
+   Icon
+] = bulk(
+   filters.byProps("useSettingScreen"),
+   filters.byProps("useSettingTitle"),
+   filters.byName("SettingsOverviewScreen", false),
+   filters.byProps("getCurrentUser", "getUser"),
+   filters.byName("getScreens"),
+   filters.byProps("Messages"),
+   filters.byName("Icon")
+)
 
-const SilentTyping: Plugin = {
-   ...manifest,
-
-   onStart() {
-      Patcher.instead(Typing, 'startTyping', () => { });
-      Patcher.instead(Typing, 'stopTyping', () => { });
+const styles = StyleSheet.createThemedStyleSheet({
+   form: {
+      width: "100%",
+      borderRadius: 16,
+      backgroundColor: Constants.ThemeColorMap.BACKGROUND_PRIMARY
    },
-
-   onStop() {
-      Patcher.unpatchAll();
-   },
-
-   getSettingsPanel({ settings }) {
-      return <Settings settings={settings} />;
+   icon: {
+      width: 16,
+      height: 16,
+      tintColor: "#fff",
+      margin: 8
    }
-};
+})
 
-registerPlugin(SilentTyping);
+const EnmityYou: Plugin = { 
+   ...manifest, 
+   onStart() {
+      Patcher.after(Screens, "useSettingScreens", (_, __, res) => {
+         const { Enmity, EnmityPlugins, EnmityThemes } = getScreens(UserStore.getCurrentUser());
+         res = { ...res }
+         
+         Object.assign(res, { 
+            ENMITY: {
+               route: "Enmity",
+               getComponent: () => Enmity.render
+            },
+            ENMITY_PLUGINS: {
+               route: "Plugins",
+               getComponent: () => EnmityPlugins.render
+            },
+            ENMITY_THEMES: {
+               route: "Themes",
+               getComponent: () => EnmityThemes.render
+            },
+            ENMITY_PAGE: {
+               route: "EnmityCustomPage",
+               getComponent: () => ({ route: { params: { pagePanel } } }) => {
+                  const Component = pagePanel ?? View;
+                  return <Component />;
+               },
+            }
+         })
+
+         return res
+      })
+
+      Patcher.after(Titles, "useSettingTitles", (_, __, res) => {
+         res = { ...res }
+         
+         Object.assign(res, { 
+            ENMITY: "General",
+            ENMITY_PLUGINS: "Plugins",
+            ENMITY_THEMES: "Themes",
+            ENMITY_PAGE: "Subpage"
+         })
+
+         return res
+      })
+
+      Patcher.after(SettingsOverviewScreen, "default", (_, [{ navigation }], { props: { children: res }}) => {
+         const { children } = findInReactTree(res, r => r.children[1].type === FormSection);
+         const index = children.findIndex(c => c.props.title === Locale.Messages.PREMIUM_SETTINGS_GENERIC);
+         const { ENMITY: GENERAL, ENMITY_PLUGINS: PLUGINS, ENMITY_THEMES: THEMES } = Screens.useSettingScreens();
+         const { ENMITY, ENMITY_PLUGINS, ENMITY_THEMES } = Titles.useSettingTitles();
+
+         children.splice(index === -1 ? 1 : index, 0, (
+            <FormSection 
+               key='Enmity' 
+               title='Enmity' 
+               inset
+            >
+               <View style={styles.form}>
+                  <FormRow
+                     label={ENMITY}
+                     leading={() => (
+                        <Icon 
+                           source={{ uri: 'https://files.enmity.app/icon-64.png' }}
+                           style={styles.icon} 
+                        />
+                     )}
+                     trailing={<FormArrow />}
+                     onPress={() => void navigation.push(GENERAL.route)}
+                  />
+                  <FormDivider />
+                  <FormRow
+                     label={ENMITY_PLUGINS}
+                     leading={() => (
+                        <Icon 
+                           source={getIDByName("ic_activity_24px")} 
+                           style={styles.icon}
+                        />
+                     )}
+                     trailing={<FormArrow />}
+                     onPress={() => void navigation.push(PLUGINS.route)}
+                  />
+                  <FormDivider />
+                  <FormRow
+                     label={ENMITY_THEMES}
+                     leading={() => (
+                        <Icon 
+                           source={getIDByName("img_nitro_star")} 
+                           style={styles.icon}
+                        />
+                     )}
+                     trailing={<FormArrow />}
+                     onPress={() => void navigation.push(THEMES.route)}
+                  />
+               </View>
+            </FormSection>
+         ));
+      })
+   }, 
+   
+   onStop() { 
+      Patcher.unpatchAll(); 
+   }, 
+}; 
+
+registerPlugin(EnmityYou);
