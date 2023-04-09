d1a712ed8bb821d32d1dc6695b6d9f1e33fd8f6e
Acquite <acquitefx@gmail.com>
2023-04-09 05:38:17 +0100

    Alter instead hook function slightly

--- src/insteadPatchHook.ts
+++ src/insteadPatchHook.ts
@@ -4,12 +4,12 @@ import { data } from './data';
 
 export default (Patcher: Patcher, object: any, func: string, type: string) => {
     Patcher.instead(object, func, (self, args, orig) => {
-        const wanted = findInTree(
+        const predicate = findInTree(
             data, node => node.upper === args[0], 
-            { walkable: [ "base", "plugins", "themes" ] }
+            { walkable: Object.keys(data) }
         );
 
-        if (!wanted) return orig.apply(self, args);
-        return wanted[type];
+        if (!predicate) return orig.apply(self, args);
+        return predicate[type];
     });
 };
